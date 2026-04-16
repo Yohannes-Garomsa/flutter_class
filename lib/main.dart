c4f4f07de644e9fa0adbed5c159db7a1b5edbbf7
Yohannes-Garomsa <yohannesgaromsa051@gmail.com>
2026-04-16 15:09:11 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 35, 23, 250),
+                Color.fromARGB(255, 14, 246, 188),
+              ],
+
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello, Flutter!',
+              style: TextStyle(
+                color: Color.fromARGB(240, 255, 255, 255),
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
