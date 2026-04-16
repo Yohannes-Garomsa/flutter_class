4e45c5869daddba5424518a93edfe1cb04ec50f9
Yohannes-Garomsa <yohannesgaromsa051@gmail.com>
2026-04-16 16:00:55 +0300

    i separated  gradient container

--- lib/main.dart
+++ lib/main.dart
@@ -3,33 +3,3 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(home: Scaffold(body: GradientContainer())));
 }
-
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 35, 23, 250),
-            Color.fromARGB(255, 14, 246, 188),
-          ],
-
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          'Hello, Flutter!',
-          style: TextStyle(
-            color: Color.fromARGB(240, 255, 255, 255),
-            fontSize: 28,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    );
-  }
-}
