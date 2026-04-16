f42cd37cc9f25774adbffc7464dae264bd6a21e0
Yohannes-Garomsa <yohannesgaromsa051@gmail.com>
2026-04-16 22:07:58 +0300

    sizebox and padding added for customizing button

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -25,9 +25,11 @@ class GradientContainer extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           children: [
             Image.asset('assets/images/dice1.png', width: 200),
+            const SizedBox(height: 40),
             TextButton(
               onPressed: rollDice,
               style: TextButton.styleFrom(
+                padding: const EdgeInsets.only(top: 50),
                 foregroundColor: Colors.white,
                 textStyle: const TextStyle(
                   fontSize: 28,
